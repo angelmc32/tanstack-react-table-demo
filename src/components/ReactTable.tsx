4b63ac8539eb7026d5df59245b29a68c9c01380b
Mel <angel.melendez.c@gmail.com>
2023-08-18 08:58:02 -0600

    Setup tailwind, create first table

--- src/components/ReactTable.tsx
+++ src/components/ReactTable.tsx
@@ -1,5 +1,180 @@
+import { useMemo, useState } from "react";
+import mockData from "../DATA.json";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+type TColumnHelper = {
+  id: number;
+  last_updated: string;
+  name: string;
+  artist: string;
+  team: string;
+  influencers: number;
+  status: string;
+  paymentId: number;
+  videoId: number;
+  statsId: number;
+};
+
 const ReactTable = () => {
-  return <div>ReactTable</div>;
+  const data: TColumnHelper[] = useMemo(() => mockData, []);
+  const columnHelper = createColumnHelper<TColumnHelper>();
+
+  const columns = [
+    columnHelper.accessor("last_updated", {
+      id: "S.No",
+      cell: (info) => <span>{info.row.index + 1}</span>,
+      header: "S.No",
+    }),
+    columnHelper.accessor("name", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "First Name",
+    }),
+    columnHelper.accessor("artist", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Last Name",
+    }),
+    columnHelper.accessor("team", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Age",
+    }),
+    columnHelper.accessor("influencers", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Visits",
+    }),
+    columnHelper.accessor("status", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Progress",
+    }),
+    columnHelper.accessor("paymentId", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Progress",
+    }),
+    columnHelper.accessor("videoId", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Progress",
+    }),
+    columnHelper.accessor("statsId", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Progress",
+    }),
+  ];
+
+  const [globalFilter, setGlobalFilter] = useState("");
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      globalFilter,
+    },
+    getFilteredRowModel: getFilteredRowModel(),
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
+  return (
+    <div className="p-2 max-w-5xl mx-auto text-white fill-gray-400">
+      <table className="border border-gray-700 w-full text-left">
+        <thead className="bg-indigo-600">
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id} className="capitalize px-3.5 py-2">
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.length ? (
+            table.getRowModel().rows.map((row, i) => (
+              <tr
+                key={row.id}
+                className={`
+                ${i % 2 === 0 ? "bg-gray-900" : "bg-gray-800"}
+                `}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id} className="px-3.5 py-2">
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))
+          ) : (
+            <tr className="text-center h-32">
+              <td colSpan={12}>No Recoard Found!</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      {/* pagination */}
+      <div className="flex items-center justify-end mt-2 gap-2">
+        <button
+          onClick={() => {
+            table.previousPage();
+          }}
+          disabled={!table.getCanPreviousPage()}
+          className="p-1 border border-gray-300 px-2 disabled:opacity-30"
+        >
+          {"<"}
+        </button>
+        <button
+          onClick={() => {
+            table.nextPage();
+          }}
+          disabled={!table.getCanNextPage()}
+          className="p-1 border border-gray-300 px-2 disabled:opacity-30"
+        >
+          {">"}
+        </button>
+
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{" "}
+            {table.getPageCount()}
+          </strong>
+        </span>
+        <span className="flex items-center gap-1">
+          | Go to page:
+          <input
+            type="number"
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              table.setPageIndex(page);
+            }}
+            className="border p-1 rounded w-16 bg-transparent"
+          />
+        </span>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={(e) => {
+            table.setPageSize(Number(e.target.value));
+          }}
+          className="p-2 bg-transparent"
+        >
+          {[10, 20, 30, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default ReactTable;
